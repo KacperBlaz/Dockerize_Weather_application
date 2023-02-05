6b27ac6c7c3c08c8cd9a17b37497a032499d8d39
ZBLAKAC <kacperblaz@gmail.com>
2023-02-05 15:41:43 +0000

    fixing csv_convert function

--- weather_app/weather_app.py
+++ weather_app/weather_app.py
@@ -18,9 +18,9 @@ print(get_api_data())
 # CELCIUS = Calvins - 273.15
 #CONVERTING CALVINS TO CELCIUS
 def args_from_output(api_response: dict) -> list:
-        city = api_response['name']
-        temperature = api_response['main']['temp']
-        return [city, convert_temperature(temperature)]
+    city = api_response['name']
+    temperature = api_response['main']['temp']
+    return [city, convert_temperature(temperature)]
 
 def convert_temperature(temperature: str) -> str:
     return str(round(temperature - 273.15, 1)) + "℃"
@@ -31,11 +31,10 @@ print(args_from_output(get_api_data()))
 #ADDING DATA FROM FUNCTION args_from_output TO CSV FILE
 def add_data_to_csv_file(csv_file, args):
     with open(csv_file,'w', newline="") as file:
-        header = ['Name','Temperature']
-        imported_data = args[0], args[1]
-        csvwriter = csv.writer(file)
-        csvwriter.writerow(header)
-        csvwriter.writerow(imported_data)
+        fieldnames = ['Name', 'Temperature']
+        writer = csv.DictWriter(file, fieldnames=fieldnames)
+        writer.writeheader()
+        writer.writerow({"Name": args[0], "Temperature": args[1]})
 
 
 print(add_data_to_csv_file('data.csv', args_from_output(get_api_data())))
