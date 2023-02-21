7eac61d5d3f1f8641febbaa5c33080af5b14b9a4
Kacper <kacperblaz@gmail.com>
2023-02-21 15:53:44 +0000

    adding database to MYSQL container

--- weather_app/weather_app.py
+++ weather_app/weather_app.py
@@ -3,34 +3,34 @@ import requests
 import json
 import csv
 import math
-import csv
+import pymysql
 
 
 API_KEY = os.environ.get('API_KEY')
 CITY = os.environ.get('CITY')
 MYSQL_ROOT_LOGIN = os.environ.get('MYSQL_ROOT_LOGIN')
 MYSQL_ROOT_PASSWORD = os.environ.get('MYSQL_ROOT_PASSWORD')
-DATABASE = 'weather'
+DATABASE = os.environ.get('DATABASE')
 FILE = 'data.csv'
 TABLE_NAME = os.environ.get('TABLE_NAME')
 DATABASE_IP = os.environ.get('DATABASE_IP')
 
 
 #TAKING DATA FROM API FOR SELECTED CITY
-def get_api_data():
-    api_endpoint= (f"http://api.openweathermap.org/data/2.5/weather?q={CITY}&appid={API_KEY}")
-    print(api_endpoint)
+def get_api_data(city, api_key: str) -> dict:
+    api_endpoint= (f"http://api.openweathermap.org/data/2.5/weather?q={city}&appid={api_key}")
     request_url = requests.get(api_endpoint)
-    print(api_endpoint)
     if request_url.status_code == 200:
         data = json.loads(request_url.text)
         return data
+
 
 #CONVERTING CALVINS TO CELCIUS
 def args_from_output(api_response: dict) -> list:
     city = api_response['name']
     temperature = api_response['main']['temp']
     return [city, convert_temperature(temperature)]
+
 
 def convert_temperature(temperature: str) -> str:
     return str(round(temperature - 273.15, 1)) + "℃"
@@ -44,12 +44,31 @@ def add_data_to_csv_file(csv_file, args):
         writer.writeheader()
         writer.writerow({"Name": args[0], "Temperature": args[1]})
 
+
+#CREATING MYSQL DATABASE IN CONTAINER
+def create_mysq_database(database_name: str) -> str:
+        mydb = pymysql.connect(
+        host = (f"{DATABASE_IP}"),
+        user = (f"{MYSQL_ROOT_LOGIN}"),
+        password = (f"{MYSQL_ROOT_PASSWORD}")
+        )
+        mycursor = mydb.cursor()
+        try:
+            mycursor.execute(f"CREATE DATABASE {database_name}")
+            print(f"Database {database_name} has been created !")
+        except pymysql.err.ProgrammingError:
+            print(f"Error, database {database_name} exist! Skipping")
+
+
+#CREATING A TABLE FROM CSV FILE
 def import_csv_file_to_mysql_database(csv_file: str) -> str:
     add_table_to_database = (f"csvsql --db mysql+pymysql://{MYSQL_ROOT_LOGIN}:{MYSQL_ROOT_PASSWORD}@{DATABASE_IP}/{DATABASE} --tables "
     f"{TABLE_NAME} --insert {csv_file}")
     os.system(add_table_to_database)
 
-get_api_data()
-print(args_from_output(get_api_data()))
-add_data_to_csv_file(FILE, args_from_output(get_api_data()))
+
+#RUNNING FUNCTIONS
+create_mysq_database(DATABASE)
+print(args_from_output(get_api_data(CITY, API_KEY)))
+add_data_to_csv_file(FILE, args_from_output(get_api_data(CITY, API_KEY)))
 import_csv_file_to_mysql_database(FILE)
